c6e691b846edfb6e41d12515c4ad013abf90bfd7
kunikyu <106215442+kunikyu@users.noreply.github.com>
2025-10-16 11:52:10 +0900

    update 2 files and create 2 files

--- sql/03/init-s_users.sql
+++ sql/03/init-s_users.sql
@@ -6,10 +6,13 @@ CREATE TABLE s_users (id INT PRIMARY KEY, name TEXT NOT NULL, age INT);
 
 -- テーブルにレコード（ユーザ情報）を挿入する
 INSERT INTO
-  s_users (id, name, age)
+    s_users (id, name, age)
 VALUES
-  (1, 'Alice', 20),
-  (2, 'Bob', 25);
+    (1, 'Alice', 20),
+    (2, 'Bob', 25);
 
 -- すべてのレコードを抽出する
-SELECT * FROM s_users;
+SELECT
+    *
+FROM
+    s_users;
